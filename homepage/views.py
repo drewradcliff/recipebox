74b273d71dba5e4bb59ad3cc9258b3fafd31e6c2
Drew Radcliff <sirampr@gmail.com>
2020-08-06 13:06:32 -0400

    Initial commit

---
+++ homepage/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from homepage.models import Recipe, Author
+
+# Create your views here.
+
+
+def index(request):
+    recipes = Recipe.objects.all()
+    return render(request, "index.html", {"recipes": recipes})
+
+
+def recipe(request, recipe_id):
+    recipe = Recipe.objects.filter(id=recipe_id).first()
+    return render(request, "recipe.html", {"recipe": recipe})
+
+
+def author(request, author_id):
+    author = Author.objects.filter(id=author_id).first()
+    author_recipes = Recipe.objects.filter(id=author_id)
+    return render(request, "author.html", {"author": author, "author_recipes": author_recipes})
